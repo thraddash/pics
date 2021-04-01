8ce25cc5d172d4e894a21dbc08dae2236f00e27c
thraddash <theartist@gmail.com>
2021-04-01 12:55:56 -0700

    add event.preventDefault() disable  form from auto submit/refresh

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,26 @@
 import React from 'react';
-
-// Class base component
+/* Class base component - controlled
+User types input
+Callback onChange invoked
+pulled current value from event object (e) by referencing event.target.value 
+update the setState term with that value
+render method is automatically re-render when setState is called
+when component re-renders, the value prop of the input tag is going to be overwritten
+*/
 
 class SearchBar extends React.Component {
     state = { term: '' };
-    // refactor, store value in component than in DOM
+    // disable page from automatically submitting or refreshing
+    // add onSubmit event handler with reference callback method this.onFormSubmit
+    // add event.preventDefault()
+    onFormSubmit(event) {
+      event.preventDefault();
+    }
+
     render() {
         return (
           <div className="ui segment">
-              <form className="ui form">
+              <form onSubmit={this.onFormSubmit} className="ui form">
                   <div className="field">
                     <label>Image Search</label>
                     <input type="text" 
@@ -16,7 +28,7 @@ class SearchBar extends React.Component {
                       onChange={e => this.setState({ term: e.target.value })} 
                     />
                   </div>
-              </form>
+              </form> 
           </div>
         );
     }
