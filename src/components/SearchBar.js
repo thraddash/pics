2d0085deb897ed810166c409ede07bd1022452c6
thraddash <theartist@gmail.com>
2021-03-31 22:01:16 -0700

    convert to controlled element

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,18 +3,18 @@ import React from 'react';
 // Class base component
 
 class SearchBar extends React.Component {
-    //create callback onChange ref onInputChange
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
-
+    state = { term: '' };
+    // refactor, store value in component than in DOM
     render() {
         return (
           <div className="ui segment">
               <form className="ui form">
                   <div className="field">
                     <label>Image Search</label>
-                    <input type="text" onChange={this.onInputChange} />
+                    <input type="text" 
+                      value = {this.state.term} 
+                      onChange={e => this.setState({ term: e.target.value})} 
+                    />
                   </div>
               </form>
           </div>
