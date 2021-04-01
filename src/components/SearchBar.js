34961fedd2183de9b97a3d888cbb34077ac1a095
thraddash <theartist@gmail.com>
2021-03-31 18:41:08 -0700

    create callback onChange ref onInputChange, console.log

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,13 +3,18 @@ import React from 'react';
 // Class base component
 
 class SearchBar extends React.Component {
+    //create callback onChange ref onInputChange
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
     render() {
         return (
           <div className="ui segment">
               <form className="ui form">
                   <div className="field">
                     <label>Image Search</label>
-                    <input type="text" />
+                    <input type="text" onChange={this.onInputChange} />
                   </div>
               </form>
           </div>
