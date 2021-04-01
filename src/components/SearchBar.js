58112d023d64b61e88489d71903a398b7f797e5c
thraddash <theartist@gmail.com>
2021-03-31 18:59:38 -0700

    alternative event handler onChange, without creating additional method onInputChange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,7 +14,7 @@ class SearchBar extends React.Component {
               <form className="ui form">
                   <div className="field">
                     <label>Image Search</label>
-                    <input type="text" onChange={this.onInputChange} />
+                    <input type="text" onChange={(e) => console.log(e.target.value)} />
                   </div>
               </form>
           </div>
