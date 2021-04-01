99f9f79a350c3a0f1cf10f4fb00615072491288e
thraddash <theartist@gmail.com>
2021-03-31 18:44:14 -0700

    test callback onClick

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,13 +8,17 @@ class SearchBar extends React.Component {
         console.log(event.target.value);
     }
 
+    onInputClick() {
+        console.log('Input was clicked');
+    }
+
     render() {
         return (
           <div className="ui segment">
               <form className="ui form">
                   <div className="field">
                     <label>Image Search</label>
-                    <input type="text" onChange={this.onInputChange} />
+                    <input type="text" onClick={this.onInputClick} onChange={this.onInputChange} />
                   </div>
               </form>
           </div>
