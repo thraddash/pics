8d212b242b07aa243cb60f85e42850e84148ae04
thraddash <theartist@gmail.com>
2021-04-01 20:29:34 -0700

    install axios, plug-in unsplash api query, require dotenv for api key

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
+require('dotenv').config({ path: './../../.env' })
 
 // refactor into class base component
 // take onSearchSubmit method and pass it down to SearchBar as a prop
 // prop can only go down
+const api_key = process.env.REACT_APP_UNSPLASH_KEY;
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID ' + api_key
+      }  
+    });
   }
 
   render() {
