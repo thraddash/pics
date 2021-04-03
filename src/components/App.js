f02f31c80101950c2c54b6645f612d0f1ad426d9
thraddash <theartist@gmail.com>
2021-04-02 23:34:32 -0700

    use async await for axios promise

--- src/components/App.js
+++ src/components/App.js
@@ -9,23 +9,25 @@ require('dotenv').config({ path: './../../.env' })
 // prop can only go down
 const api_key = process.env.REACT_APP_UNSPLASH_KEY;
 
-// option 1 chaining .then() function
+// option 1: chaining .then() function (asynchronous)
 // Axios returns an object call promise. 
 // a promise will give a notification when some amount of work is completed like a request
 // To get an indication that the request is completed use .then() function
 // pass an arrow function inside .then function, so a callback will be invoked with whatever data
-// from unsplash api  
+// from unsplash api .then((response) => {console.log(response.data.results);}); 
+
+// option 2: add aysnc infront of onSearchSubmit() function name
+// allows usage syntax await
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID ' + api_key
       }  
-    }).then((response) => {
-      console.log(response.data.results);
     });
+    console.log(response.data.results);
   }
 
   render() {
