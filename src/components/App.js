36110d47ba37f9e164976037a9215d41f74701de
thraddash <theartist@gmail.com>
2021-04-02 23:48:00 -0700

    this.setState is not a function issue

--- src/components/App.js
+++ src/components/App.js
@@ -16,10 +16,14 @@ const api_key = process.env.REACT_APP_UNSPLASH_KEY;
 // pass an arrow function inside .then function, so a callback will be invoked with whatever data
 // from unsplash api .then((response) => {console.log(response.data.results);}); 
 
-// option 2: add aysnc infront of onSearchSubmit() function name
-// allows usage syntax await
+// option 2: add aysnc keyword infront of onSearchSubmit() function name
+// allows usage syntax await keyword in front network request
 
 class App extends React.Component {
+  //set api data into a component state, to cause the component to re-render
+  // create new state with images property, empty array
+  state = { images: [] };
+
   async onSearchSubmit(term) {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
@@ -27,13 +31,17 @@ class App extends React.Component {
         Authorization: 'Client-ID ' + api_key
       }  
     });
-    console.log(response.data.results);
+    // pass list of results to state object "images", causing component to re-render
+    // print out images count 
+    console.log(this);
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     )
     }
