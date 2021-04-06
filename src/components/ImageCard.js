62e422dc1814b7cab3a0203addb946f1d03b8567
thraddash <theartist@gmail.com>
2021-04-05 23:40:10 -0700

    add review notes

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -17,7 +17,8 @@ class ImageCard extends React.Component {
 
     componentDidMount(){
 // create event listner, and a callback this.setSpan
-// callback must be found or it will be undefine, use arrow function in setSpans 
+// callback must be found or it will be undefine, use arrow function in setSpans
+// automatically bind the function to make sure it always have the property of "this" inside of it
 // console.log(this.imageRef);
 // console.log(this.imageRef.current.clientHeight);
         this.imageRef.current.addEventListener('load',  this.setSpans);
@@ -46,3 +47,28 @@ class ImageCard extends React.Component {
 }
 
 export default ImageCard;
+
+/* 
+Review
+SearchBar
+eventhandler, input tag, form tag, use callback function {this.onFormSubmit}
+to a specifically named prop onSubmit, so when the element or form emits the event
+the callback function will be called
+issue that may run into is value of "this" maybe undefine
+use arrow function syntax, automatically binds the func to make sure the value of "this" is inside the func
+
+with props system, only can communicate from parent to child
+from child to parent
+create callback function in parent component. this.props.onSubmit(this.state.term)
+This callback function will get data from the child component
+pass the callback function in the parent as a prop to the child component
+child component calls the parent callback function using props
+
+map function for ImageList.js, add id to key property for each image 
+<ImageCard key={image.id}
+
+ImageCard.js
+interact with DOM element to make reference to JSX element <img , create a Ref inside a constructor
+this.imageRef = React.createRef();
+wire it up to an individual element by passing it as a ref property
+<img ref={this.imageRef}
