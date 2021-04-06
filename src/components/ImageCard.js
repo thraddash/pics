808479ec364729beb77b88dafa61a827a67e067a
thraddash <theartist@gmail.com>
2021-04-05 22:41:20 -0700

    use React refs to access DOM elment img clientHeight to resize image

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
 // create one image by itself
+// React Refs, gives direct access to DOM element 
+// create refs in constructor, assign them to instance variables
+// pass to JSX element as props
+
 class ImageCard extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount(){
+        console.log(this.imageRef);
+    }
+
     render(){
         const { description, urls } = this.props.image;
+
         return(
           <div>
-              <img alt={description} src={urls.regular} />
+              <img ref={this.imageRef} alt={description} src={urls.regular} />
           </div>  
         );
     }
